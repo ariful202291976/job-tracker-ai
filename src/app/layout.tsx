6b136711858501b6e7f2f0f948d7c59777a69ff4
ariful202291976 <marifuli@mun.ca>
2026-01-31 11:12:09 -0330

    create register api route and session provider

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
-import { Toaster } from "@/components/ui/sonner";
+import AuthProvider from "@/components/providers/session-provider";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -18,8 +18,9 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
+        <AuthProvider>
           {children}
-          <Toaster position="top-right" richColors />
+        </AuthProvider>
       </body>
     </html>
   );
